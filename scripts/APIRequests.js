76aab61e66e17847eff85220f738f9a8f0f9503f
Jean-Sébastien Merz <jsmpub@gmail.com>
2019-02-06 17:31:58 +0100

    Jean-Seb - fonctionnel avec chargement fichier par parcourir

--- scripts/APIRequests.js
+++ scripts/APIRequests.js
@@ -1,52 +1,39 @@
 const visionApiURL = "https://francecentral.api.cognitive.microsoft.com/vision/v1.0/";
 function VisionAPIPost(action, file) {
 
+    var reader = new FileReader();
 
-        var reader = new FileReader();
-/*
-        reader.onload = function() {
-            fetch("", {
-                method : "post",
-                headers : {"Content-Type":"application/octet-stream",
-                    "Ocp-Apim-Subscription-Key":"3dd27f3728084d4b9e487d6b0390b7c4"
-                },
-                body : reader.result
-            }).then(response => {
-                console.log(response);
-            }).catch(error=> {
-                console.log(error)})
-        };
+            reader.onload = function() {
+                const req = new XMLHttpRequest();
+                let url = visionApiURL + action;
+                req.onreadystatechange = function (event) {
+                    if (this.readyState === XMLHttpRequest.DONE) {
+                        if (this.status === 200) {
+                            console.log("Statuts de la réponse: %d (%s)", this.status, this.statusText);
 
-        reader.readAsArrayBuffer(file);
-    }
-*/
+                            result = JSON.parse(this.responseText);
+
+                            displayAnalysis(action, result);
+                        } else {
+                            console.log("Statuts de la réponse: %d (%s)", this.status, this.statusText);
+                        }
+                    }
+                };
+                let formData = new FormData();
+
+                formData.append("file", file);
+                req.open('POST', url, true);
+                req.setRequestHeader("Ocp-Apim-Subscription-Key", "3dd27f3728084d4b9e487d6b0390b7c4");
+                req.setRequestHeader("Content-Type", "application/octet-stream");
+                req.send(reader.result);
+            };
+
+            reader.readAsArrayBuffer(file);
 
 
 
-    const  req = new XMLHttpRequest();
-    let url = visionApiURL+action;
-    let body= {"url":"https://images.pexels.com/photos/145939/pexels-photo-145939.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"};
-    req.onreadystatechange = function (event) {
-        if (this.readyState === XMLHttpRequest.DONE) {
-            if (this.status === 200) {
-                console.log("Statuts de la réponse: %d (%s)", this.status, this.statusText);
 
-                result = JSON.parse(this.responseText);
 
-                displayAnalysis(action, result);
-                console.log(result);
 
-            } else {
-                console.log("Statuts de la réponse: %d (%s)", this.status, this.statusText);
-            }
-        }
-    };
-    let formData = new FormData();
-
-    formData.append("file",file);
-    console.log(formData);
-    req.open('POST', url, true);
-    req.setRequestHeader("Ocp-Apim-Subscription-Key","3dd27f3728084d4b9e487d6b0390b7c4");
-    req.setRequestHeader("Content-Type","application/octet-stream");
-    req.send(reader.result);
+}
 
--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,33 @@
+const acceptedFileTypes = ["image/jpeg", "image/png", "image/gif", "image/bmp"];
+
+
 window.addEventListener("load", function() {
     document.getElementById("image_input").addEventListener("change", function(){
         let file = this.files[0];
-        console.log(file);
+        //Vérification des caractéristiques du fichier
+        console.log(file.type);
+        console.log(acceptedFileTypes.includes(file.type));
+        if (acceptedFileTypes.includes(file.type)){
+            console.log("mauvais type de fichier");
+            return;
+        }
+        //affichage du fichier
+        let reader = new FileReader();
+        let divImageDisplay = document.getElementById("imageDisplay")
+        reader.onload = function (e) {
+            //On vérifie s'il y a déjà une image
+            if (divImageDisplay.querySelector("img") == null) {
+                //On crée l'élément image
+                let img = document.createElement("img");
+                img.setAttribute("id", "analyzed_picture");
+                divImageDisplay.appendChild(img);
+            }
+            document.getElementById("analyzed_picture").setAttribute("src",e.target.result)
+        }
+        reader.readAsDataURL(file);
+
+
+        //Envoie le fichier à l'API
         VisionAPIPost("describe", file);
 
     });
